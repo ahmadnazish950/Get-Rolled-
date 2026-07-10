0fe5dfc3037c32a557ed305b2598537808c83b2a
Nazish Ahmad <ahmadnazish950@gmail.com>
2026-07-10 14:29:03 +0530

    Configure backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connect = require('./src/db/db')
+require("dotenv").config();
 
-connect()
+const app = require("./src/app");
+const connect = require("./src/db/db");
 
-const PORT = process.env.PORT || 3000
+connect();
 
-app.listen(PORT, () => {
-    console.log(`server was running on ${PORT}`)
-})
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
+
+module.exports = app;
